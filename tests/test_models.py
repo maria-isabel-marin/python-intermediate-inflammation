3e37acdbafe8ec6cff5b68fe6fe2f187435290c7
MARIA ISABEL MARIN <mimarinm@gmail.com>
2023-12-12 09:22:46 +0100

    Add defensing programming and corner cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -66,8 +66,8 @@ def test_daily_min_string():
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
     ])
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
@@ -78,9 +78,9 @@ def test_daily_mean(test, expected):
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
-        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [4, 6, 9]),
-        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [4, -1, 9]),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [4, 6, 9]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [4, -1, 9]),
     ])
 def test_daily_max(test, expected):
     """Test max function works for zeroes, positive integers, mix of positive/negative integers."""
@@ -91,11 +91,66 @@ def test_daily_max(test, expected):
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
-        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [1, 1, 2]),
-        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [-4, -6, 2]),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [1, 1, 2]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [-4, -6, 2]),
     ])
 def test_daily_min(test, expected):
     """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                None,
+        ),
+        (
+                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+                None,
+        ),
+        (
+                [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+                [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+                None,
+        ),
+        (
+                [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+                None,
+        ),
+        (
+                [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                ValueError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        ),
+        (
+                [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                ValueError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        )
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
